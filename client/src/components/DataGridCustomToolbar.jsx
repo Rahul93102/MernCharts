a30e01d28b4ba51156ce5aa84d9ccbac3054e98a
Rahul Jha <132360698+Rahul93102@users.noreply.github.com>
2025-08-22 16:02:29 +0530

    Toolbar modoification

--- client/src/components/DataGridCustomToolbar.jsx
+++ client/src/components/DataGridCustomToolbar.jsx
@@ -44,4 +44,19 @@ const DataGridCustomToolbar = ({ searchInput, setSearchInput, setSearch }) => {
   );
 };
 
+
+const CustomColumnMenu = (props) => {
+  const { hideMenu, currentColumn, open } = props;
+  return (
+    <GridColumnMenuContainer
+      hideMenu={hideMenu}
+      currentColumn={currentColumn}
+      open={open}
+    >
+      <GridFilterMenuItem onClick={hideMenu} column={currentColumn} />
+      <HideGridColMenuItem onClick={hideMenu} column={currentColumn} />
+    </GridColumnMenuContainer>
+  );
+};
+
 export default DataGridCustomToolbar;
